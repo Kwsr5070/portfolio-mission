27d679e10707c11431e63f344b8696a02ce15054
md kawser <mdkwsr5070@gmail.com>
2024-05-25 22:43:23 +0600

    fifth added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      // Specify the alias for src/main.jsx
+      '@/src/main.jsx': '/path/to/src/main.jsx', // Replace '/path/to/src/main.jsx' with the actual path to your main.jsx file
+    },
+  },
+});
